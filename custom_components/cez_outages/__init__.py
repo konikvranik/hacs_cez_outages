e8aa2ad136e5eb5955a5d07e6a9044bf4ea93f38
pvranik <petr.vranik@mgm-tp.com>
2021-04-14 12:53:15 +0200

    path to manifest

--- custom_components/cez_outages/__init__.py
+++ custom_components/cez_outages/__init__.py
@@ -1,6 +1,7 @@
 import datetime
 import json
 import logging
+import os
 
 import requests
 import voluptuous as vol
@@ -17,7 +18,7 @@ CONF_STREET = "street"
 CONF_REFRESH_RATE = 'refreshRate'
 CONF_MAX_COUNT = 'maxCount'
 
-MANIFEST = json.load(open("mainfest.json"))
+MANIFEST = json.load(open("%s/mainfest.json" % os.path.dirname(os.path.realpath(__file__))))
 DOMAIN = MANIFEST["domain"]
 VERSION = MANIFEST["version"]
 DEFAULT_NAME = MANIFEST["name"]
